28d8a86efa6dd997fcb9b3d9abf70c920ef44c1d
VMHDev <vmhpublic2017@gmail.com>
2021-05-14 10:13:12 +0700

    Test connect database with api login and register

---
+++ src/api/userApi.js
@@ -0,0 +1,12 @@
+import axiosClient from './axiosClient';
+const userApi = {
+  register: (data) => {
+    const url = '/user/register';
+    return axiosClient.post(url, data);
+  },
+  login: (data) => {
+    const url = '/auth/login';
+    return axiosClient.post(url, data);
+  },
+};
+export default userApi;
--- src/constants/system.js
+++ src/constants/system.js
@@ -10,3 +10,10 @@ export const WEB_PORT = process.env.REACT_APP_PORT;
 export const WEB_URL = WEB_DOMAIN + WEB_PORT;
 
 export const COOKIES_EXPIRE = process.env.REACT_APP_COOKIES_EXPIRE;
+
+export const API_URL =
+  process.env.NODE_ENV !== 'production'
+    ? process.env.REACT_APP_API_URL_DEV
+    : process.env.REACT_APP_API_URL_PRD;
+export const LOCAL_STORAGE_TOKEN_NAME =
+  process.env.REACT_APP_LOCAL_STORAGE_TOKEN_NAME;
