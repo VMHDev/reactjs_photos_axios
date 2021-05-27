13cc0144bfab031bc1a0eaf42b2753069e6873aa
VMHDev <vmhpublic2017@gmail.com>
2021-05-27 14:25:07 +0700

    photo: Store local storage

--- src/redux/photoSlice.js
+++ src/redux/photoSlice.js
@@ -1,20 +1,40 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import { addToLocalStorageArray } from 'utils/helper';
+
+const initPhotos = () => {
+  const photos = localStorage.getItem('photos');
+  if (photos) {
+    return JSON.parse(photos);
+  } else {
+    return { data: [] };
+  }
+};
+
+const initialState = initPhotos();
+
 const photo = createSlice({
   name: 'photos',
-  initialState: {
-    data: [],
-  },
+  initialState,
   reducers: {
     setPhoto: (state, action) => {
       state.data = action.payload;
+
+      // Store local storage
+      localStorage.setItem('photos', JSON.stringify(state));
     },
     addPhoto: (state, action) => {
       state.data.push(action.payload);
+
+      // Store local storage
+      addToLocalStorageArray('photos', action.payload);
     },
     removePhoto: (state, action) => {
       const removePhotoId = action.payload;
       state = state.data.filter((photo) => photo._id !== removePhotoId);
+
+      // Store local storage
+      localStorage.setItem('photos', JSON.stringify(state));
       return state;
     },
     updatePhoto: (state, action) => {
@@ -25,6 +45,8 @@ const photo = createSlice({
       if (photoIndex >= 0) {
         state.data[photoIndex] = udpPhoto;
       }
+      // Store local storage
+      localStorage.setItem('photos', JSON.stringify(state));
     },
   },
 });
